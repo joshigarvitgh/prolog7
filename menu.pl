3cb654b630217a82ac525f49437a366ac3a5fcca
Tom René Hennig <tom_rene.hennig@posteo.de>
2019-01-22 13:10:38 +0100

    Updated menu and data management

--- menu.pl
+++ menu.pl
@@ -1,4 +1,28 @@
-test():- writeln("G u e s s  a  W o r d"),
+%%% Database
+:- dynamic words/1.
+
+%%% Main program
+main :-
+    repeat,
+    print_menu,
+    write("Command: "),
+    get_single_char(CommandCode),
+    char_code(Command, CommandCode),
+    writeln(""), writeln(""),
+    ( Command == 'r' -> read_database ;
+      Command == 'l' -> print_database ;
+      Command == 'a' -> add_word ;
+      Command == 'd' -> delete_word ;
+      Command == 'w' -> write_database ;
+      Command == 'g' -> guess_word ;
+      Command == 'e' -> ! ;
+      writeln("Please choose one of the options.")
+    ),
+    fail.
+
+print_menu :-
+    writeln(""),
+    writeln("G u e s s  a  W o r d"),
     writeln("---------------------"),
     writeln("  r - read database file"),
     writeln("  l - list knowledge base"),
@@ -6,25 +30,43 @@ test():- writeln("G u e s s  a  W o r d"),
     writeln("  d - delete a word"),
     writeln("  w - write database file"),
     writeln("  g - guess a word"),
-    writeln("  e - end the game"),
-    repeat,
-    write("Command: "),
-    get_single_char(CommandCode),
-    char_code(Command, CommandCode),
-    switch(Command, [
-        'r' : writeln("Reading"),
-        'l' : writeln("Listing"),
-        'a' : writeln("Adding"),
-        'd' : writeln("Deleting"),
-        'w' : writeln("Writing"),
-        'g' : writeln("Guessing"),
-        'e' : writeln("Ending")
-    ]).
+    writeln("  e - end the game").
 
-switch(X, [Val:Goal|Cases]) :-
-    ( X=Val ->
-        call(Goal)
-    ;
-        switch(X, Cases)
+read_database :-
+    write("Reading database... "),
+    open("database.txt", read, DatabaseFD),
+    repeat,
+    read_string(DatabaseFD, "\n\r", " \t", End, Word),
+    (words(Word) -> true ; assert(words(Word))),    % add only new word to list
+    (End == -1 -> ! ; fail),                        % repeat until EOF
+    close(DatabaseFD),
+    writeln("Success").
+
+write_database :-
+    writeln("Writing database..."),
+    open("database.txt", write, DatabaseFD),
+    words(Word),
+    writeln(DatabaseFD, Word).
+
+print_database :-
+    writeln("Current content of the database:"),
+    words(Word),
+    writeln(Word).
+
+add_word :-
+    write("Word to add to the database: "),
+    read_string(user_input, "\n\r", " \t", _, Word),
+    (words(Word) ->
+        writeln("Word is already in the database"), true ;
+        assert(words(Word))
     ).
- 
+
+delete_word :-
+    write("Word to delete from the database: "),
+    read_string(user_input, "\n\r", " \t", _, Word),
+    (words(Word) ->
+        retractall(words(Word)) ;
+        writeln("Word is not in the database")
+    ).
+
+guess_word.
