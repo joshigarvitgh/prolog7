1f51ec076adf614a11ec010b7ddca0054898d259
Anas Shahab <34349521+AnasShahab@users.noreply.github.com>
2019-01-29 13:36:55 +0100

    Fixed game loop and indentation

--- gamef.pl
+++ gamef.pl
@@ -117,9 +117,9 @@ game(Words):-
     createGuessList(WordLen,GuessList),
     string_chars(HiddenWord,GuessList),
     write("Please guess the word: "), write(HiddenWord), nl,
-    game2(CharList,GuessList,0).
+    game2(CharList,GuessList,0),!.
     
-    game2(CharList,GuessList,NumGuess):-
+game2(CharList,GuessList,NumGuess):-
     member('*',GuessList),
     askForALetter(Letter),
     write(Letter),nl,
@@ -131,40 +131,40 @@ game(Words):-
     game2(_,_,NumGuess):-
     nl, write("Poor! It took you "), write(NumGuess), write(" guesses."), nl,!.
     
-    randomWord(Words, Word):-
+randomWord(Words, Word):-
     length(Words, Len),
     Max is Len+1,
     random(1,Max,Nth),
     getWord(Words, Nth, Word).
     
-    getWord([H|_],1,H):-!.
+getWord([H|_],1,H):-!.
     getWord([_|T],Nth,Word):-
     Nth2 is Nth-1,
     getWord(T,Nth2,Word).
     
-    askForALetter(Letter):-
+askForALetter(Letter):-
     write("Please guess a letter: "),
     get_single_char(L),
     char_code(Letter,L).
     
-    createGuessList(0,[]).
+createGuessList(0,[]).
     createGuessList(Len,GuessList):-
     Len2 is Len-1,
     createGuessList(Len2,GuessList2),
     append(['*'],GuessList2,GuessList),!.
     
-    firstHalf(_,1,[]):-!.
+firstHalf(_,1,[]):-!.
     firstHalf([H|T],Index,Result):-
     Index2 is Index - 1,
     firstHalf(T,Index2,Result2),
     append([H],Result2,Result),!.
     
-    secondHalf([_|T],1,T):-!.
+secondHalf([_|T],1,T):-!.
     secondHalf([_|T],Index,Result):-
     Index2 is Index - 1,
     secondHalf(T,Index2,Result),!.
     
-    updateGuessList(WordList,GuessList,Letter,GuessList2):-
+updateGuessList(WordList,GuessList,Letter,GuessList2):-
     nth1(Index,WordList,Letter),
     nth1(Index,GuessList,'*'),
     firstHalf(GuessList,Index,TempGuessList1),
